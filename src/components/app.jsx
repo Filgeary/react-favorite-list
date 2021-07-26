2a598d52d0ea736f666ceb1a6c11ef926fdfebfa
Georgiy Filippov <g.filippov777@gmail.com>
2021-07-26 17:13:34 +0500

    init commit

---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Learning React</h1>
+    </div>
+  );
+}
+
+export default App;
